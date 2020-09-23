cbf74cfa24a12df0b49769d34e99854358af669e
Kenneth Sibayan <kensbyn@users.noreply.github.com>
2020-09-23 13:31:27 +0800

    Update nodePrep.sh

--- scripts/nodePrep.sh
+++ scripts/nodePrep.sh
@@ -5,8 +5,8 @@ echo $(date) " - Starting Script"
 # Install EPEL repository
 echo $(date) " - Installing EPEL"
 
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+yum -y install epel-release
+#sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 
 echo $(date) " - EPEL successfully installed"
 
